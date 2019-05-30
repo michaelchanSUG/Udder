19f5d8371bf83c464b9c4a28f8aa7daae224856b
MGC <michael.chan@successug.com>
2019-05-29 22:53:54 -0700

    Add label

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by user155951 on 5/29/19.
+//  Copyright © 2019 user155951. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
